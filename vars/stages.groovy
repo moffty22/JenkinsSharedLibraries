b49f2db70736578433dade6c29998297f03c317d
moffty22 <amponsah.edward@gmail.com>
2024-06-18 14:53:54 -0500

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -2,14 +2,19 @@ def call(String stageName){
   
   if ("${stageName}" == "Build")
      {
+       sh "echo validate, compile and perform UnitTesting"
+       sh "echo UnitTesting must passed for artifacts to be created"
        sh "mvn clean package"
      }
   else if ("${stageName}" == "SonarQube Report")
      {
        sh "mvn clean sonar:sonar"
+       sh "echo CodeQualityAnalysis completed"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
        sh "mvn clean deploy"
+       sh "echo artifacts uploaded successfully"
+       sh "echo Build and release completed" 
      }
 }
