dd675fc3caf954ce628b08cb5fa79fc6a4470fdc
moffty22 <amponsah.edward@gmail.com>
2024-06-20 12:22:31 -0500

    Create njs2.groovy

---
+++ vars/njs2.groovy
@@ -0,0 +1,11 @@
+def call(String stageName) {
+    if (stageName == "Build") {
+        sh "npm install"
+    } else if (stageName == "SonarQube Report") {
+        sh "npm run sonar"
+    } else if (stageName == "Upload Into Nexus") {
+        sh "npm publish"
+    } else if (stageName == "deployment") {
+        sh "npm start"
+    }
+}
